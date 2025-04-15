56ac967c0725d2a5e8cdd8cfb2087ce2bdc3c886
Karthikeyan K <140727545+Karthikeyan260@users.noreply.github.com>
2025-04-15 20:43:26 +0530

    Delete src/app/learn-more directory

--- src/app/learn-more/page.tsx
+++
@@ -1,9 +0,0 @@
-import Learnmore from '@/components/learn-more';
-
-export default function SignInPage() {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <Learnmore />
-    </div>
-  );
-}
