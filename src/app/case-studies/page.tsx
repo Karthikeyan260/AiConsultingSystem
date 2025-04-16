854ad2a6d77ee9f175b18c65bed3745da7ddcbf1
Karthikeyan260 <kartji005@gmail.com>
2025-04-16 10:50:24 +0530

    WIP: save progress

---
+++ src/app/case-studies/page.tsx
@@ -0,0 +1,9 @@
+import CaseStudies from '@/components/case-studies';
+
+export default function ContactDeatails() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <CaseStudies />
+    </div>
+  );
+}
